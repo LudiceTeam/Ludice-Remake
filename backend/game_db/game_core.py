5598f5c162b5a306901c1953c57aff50501d8ac8
Wizer27 <svd2yar@gmail.com>
2026-04-08 22:30:56 +0300

    some game logic

--- backend/game_db/game_core.py
+++ backend/game_db/game_core.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select,update
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -40,3 +40,76 @@ async def create_table():
     async with async_engine.begin() as conn:
         await conn.run_sync(metadata_obj.create_all)
 
+
+
+async def create_or_find_game(user_id:str,bet:int) -> dict:
+    async with AsyncSession(async_engine) as conn:
+        async with conn.begin():
+            try:
+                stmt = select(
+                    game_table.c.game_id,
+                    game_table.c.player_1,
+                    
+                ).where(
+                    game_table.c.player_2.is_(None),
+                    game_table.c.player_1 != user_id,
+                    game_table.c.bet == bet
+                ).with_for_update(skip_locked=True).limit(1)
+
+                res = await conn.execute(stmt)
+
+                data = res.mappings().first()
+
+                if data is not None:
+                    stmt_update = update(game_table).where(game_table.c.game_id == data["game_id"]).values(
+                        player_2 = user_id
+                    )
+
+                    await conn.execute(stmt_update)
+                    
+                    return {
+                        "found" : True,
+                        "game_id" : data["game_id"]
+                    }
+                else:
+                    new_game_id = str(uuid.uuid4())
+                    stmt_insert = insert(game_table).values(
+                        game_id = new_game_id,
+                        player_1 = user_id,
+                        player_2 = None,
+                        bet = bet,
+                        winner = None
+                    )
+                    await conn.execute(stmt_insert)
+                    return {
+                        "found" : False,
+                        "game_id" : new_game_id
+                    }
+
+            except Exception:
+                logger.exception("GAME SQL ERROR")
+                return {}
+            
+async def delete_game(game_id:str):
+    async with AsyncSession(async_engine) as conn:
+        async with conn.begin():
+            try:
+                stmt = game_table.delete().where(game_table.c.game_id == game_id)
+                await conn.execute(stmt)
+            except Exception:
+                logger.exception("GAME SQL ERROR")
+                return
+            
+async def write_winner_to_game(game_id:str,winner:str):
+    async with AsyncSession(async_engine) as conn:
+        async with conn.begin():
+            try:
+                stmt = update(game_table).where(
+                    game_table.c.game_id == game_id
+                ).values(
+                    winner = winner
+                )
+                await conn.execute(stmt)
+            except Exception:
+                logger.exception("GAME SQL ERROR")
+                return
--- backend/game_db/games_models.py
+++ backend/game_db/games_models.py
@@ -7,7 +7,8 @@ game_table = Table(
     "game_table",
     metadata_obj,
     Column("game_id",String,primary_key = True,unique=True),
-    Column("players",ARRAY(String)),
+    Column("player_1",String),
+    Column("player_2",String),
     Column("bet",Integer),
     Column("winner",String)
 )
